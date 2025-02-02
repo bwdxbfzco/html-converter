d058acecd149c8da026f76b582f22b619365b7ed
bwdxbfzco <55143036+bwdxbfzco@users.noreply.github.com>
2025-02-02 07:28:52 +0400

    1st commit

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ app.post('/', async (req, res) => {
         });
         const page = await browser.newPage();
 
+        // Increase timeout to ensure React fully renders
+        await page.setDefaultNavigationTimeout(60000); // 60 seconds
+
         // Navigate to the given URL
         await page.goto(url, { waitUntil: 'networkidle2' });
 
