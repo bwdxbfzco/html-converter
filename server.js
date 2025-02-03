01c668fe5ba416f5fafd6978328cd481338e40a9
bwdxbfzco <55143036+bwdxbfzco@users.noreply.github.com>
2025-02-03 06:27:33 +0400

    1st commit

--- server.js
+++ server.js
@@ -94,6 +94,6 @@ app.post('/convert-to-pdf', async (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0',() => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
