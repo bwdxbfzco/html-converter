30007c75323d1f9539ff83ece1876dbc662f8fcb
bwdxbfzco <55143036+bwdxbfzco@users.noreply.github.com>
2025-02-03 05:55:22 +0400

    1st commit

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ app.post('/', async (req, res) => {
         // Navigate to the given URL
         await page.goto(url, { waitUntil: 'networkidle2' });
 
+        await page.waitForSelector('#dashboard', { timeout: 120000 });
+
+        await page.evaluate(() => new Promise(resolve => setTimeout(resolve, 5000)));
+
         // Capture full-page screenshot
         const screenshotBuffer = await page.screenshot({ fullPage: true });
 
@@ -60,10 +64,14 @@ app.post('/convert-to-pdf', async (req, res) => {
         const page = await browser.newPage();
 
         // Increase timeout to ensure React fully renders
-        await page.setDefaultNavigationTimeout(60000); // 60 seconds
+        await page.setDefaultNavigationTimeout(1200000); // 60 seconds
 
         // Navigate to the given URL
         await page.goto(url, { waitUntil: 'networkidle2' });
+
+        await page.waitForSelector('#dashboard', { timeout: 120000 });
+
+        await page.evaluate(() => new Promise(resolve => setTimeout(resolve, 5000)));
 
         // Generate a PDF across multiple pages
         const pdfBuffer = await page.pdf({
